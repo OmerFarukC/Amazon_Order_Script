9e093f8697db4b0bf2498fb3529a7b02c240f501
Faruk <Faruk@MacBook-Pro-3.local>
2024-01-08 14:19:36 +0300

    Updated script with new column order and calculations

--- address_cleanup.py
+++ address_cleanup.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import re
 
 # Read the Excel file
-df = pd.read_excel('AMZ_G_SIP.xlsx')
+df = pd.read_excel('19-12_08.01.xlsx')
 
 # Column mapping from old names to new names
 column_position_mapping = {
@@ -29,9 +29,133 @@ column_position_mapping = {
     37: 'INVOICE_COUNTY',           # Fatura Eyaleti
     38: 'INVOICE_ZIP_CODE',         # bill-postal-code
     39: 'INVOICE_COUNTRY',          # bill-country
+    40: 'PROMOSYON_PRICE',          # Ürün Promosyon İndirimi
     41: 'KARGO_PROMOSYON_INDIRIMI', # Kargo Promosyon İndirimi
-    43: 'CAROG_TRACK_NO',           # Takip Numarası
-    46: 'SALES_CHANNEL'             # Satış Kanalı
+    43: 'CARGO_TRACK_NO',           # Takip Numarası
+    47: 'SALES_CHANNEL'             # Satış Kanalı
+}
+
+sku_to_catalog = {
+    "8694407670151_fba": "10002",
+    "8694407670168_fba": "10003",
+    "8694407109200_fba": "10027",
+    "8694407100108_fba": "10014",
+    "8694407670144_fba": "10001",
+    "8694407670717_fba": "10013",
+    "8694407128775_fba": "10033",
+    "8694407128737_fba": "10028",
+    "8694407128751_fba": "10031",
+    "8694407220332_fba": "10001234",
+    "8694407160317_fba": "42001701",
+    "8694407197931_fba": "56001701",
+    "8694407734464_fba": "86111701",
+    "8694407737281_fba": "86280701",
+    "8694407149213_fba": "42001702",
+    "8694407197948_fba": "56001702",
+    "8694407204622_fba": "56050701",
+    "8694407737137_fba": "86280702",
+    "8694407734433_fba": "86111702",
+    "8694407201973_fba": "56057701",
+    "8694407139849_fba": "32001028",
+    "8694407139863_fba": "32001001",
+    "8694407160829_fba": "42001057",
+    "8694407110282_fba": "45182",
+    "8694407158338_fba": "42001011",
+    "8694407192417_fba": "56001082",
+    "8694407146113_fba": "36064099",
+    "8694407160874_fba": "43001028",
+    "8694407197863_fba": "57001028",
+    "8694407162526_fba": "43001001",
+    "8694407197832_fba": "57001001",
+    "8694407675712_fba": "45108",
+    "8694407204660_fba": "57050028",
+    "8694407707598_fba": "57054028",
+    "8694407162571_fba": "43001003",
+    "8694407197849_fba": "57001003",
+    "8694407616852_fba": "BC08.0W-0227M",
+    "8694407616944_fba": "BC10.0W-0127M",
+    "8694407596512_fba": "BC05.0W-0127M",
+    "8694407596635_fba": "BC09.0W-0127M",
+    "8694407596390_fba": "BM03.0W-0114A",
+    "8694407616821_fba": "BC06.0W-0127M",
+    "8694407596369_fba": "BM03.0W-0214A",
+    "8694407601469_fba": "DC06.0W-02005",
+    "8694407596604_fba": "BC09.0W-0227M",
+    "8694407589309_fba": "AC00.0W-07001",
+    "8694407596482_fba": "BC05.0W-0227M",
+    "8694407589330_fba": "AC00.0W-07002",
+    "8694407642271_fba": "N3201063",
+    "8694407632227_fba": "N6311053",
+    "8694407632197_fba": "N6311043",
+    "8694407642240_fba": "N3201043",
+    "8694407642363_fba": "N3301043",
+    "8694407147080_fba": "10001205",
+    "8694407593054_fba": "N4210063",
+    "8694407631954_fba": "N4311043",
+    "8694407642219_fba": "N3201033",
+    "8694407642905_fba": "N6201043",
+    "8694407592903_fba": "N6310043",
+    "8694407598103_fba": "N4310063",
+    "8694407642394_fba": "N3301063",
+    "8694407700667_fba": "10015",
+    "8694407642844_fba": "N6201000",
+    "8694407591913_fba": "N4310033",
+    "8694407721433_fba": "25064A99",
+    "8694407642691_fba": "N5301000",
+    "8694407642875_fba": "N6201033",
+    "8694407642189_fba": "N3201000",
+    "8694407672469_fba": "MGP133",
+    "8694407683915_fba": "MGP211",
+    "8694407672452_fba": "MGP132",
+    "8694407128898_fba": "63140",
+    "8694407128584_fba": "63012",
+    "8694407129796_fba": "63016",
+    "8694407128607_fba": "63024",
+    "8694407128591_fba": "63112",
+    "8694407128560_fba": "63008",
+    "8694407128577_fba": "63108",
+    "8694407128904_fba": "63141",
+    "8694407129802_fba": "63116",
+    "8694407128553_fba": "63106",
+    "8694407109910_fba": "10080",
+    "8694407109958_fba": "10081",
+    "8694407683281_fba": "18300",
+    "8694407683298_fba": "18301",
+    "8694407683328_fba": "18304",
+    "8694407707123_fba": "18308",
+    "8694407128355_fba": "18354",
+    "8694407675668_fba": "45101",
+    "8694407675682_fba": "45103",
+    "8694407108371_fba": "45104",
+    "8694407675705_fba": "45107",
+    "8694407139825_fba": "32001003",
+    "8694407144843_fba": "32001004",
+    "8694407140364_fba": "32001005",
+    "8694407139948_fba": "32001014",
+    "8694407142085_fba": "32001027",
+    "8694407140302_fba": "32001029",
+    "8694407143167_fba": "32001057",
+    "8694407149329_fba": "42001001",
+    "8694407149336_fba": "42001003",
+    "8694407149350_fba": "42001005",
+    "8694407149367_fba": "42001007",
+    "8694407149374_fba": "42001014",
+    "8694407149381_fba": "42001028",
+    "8694407149398_fba": "42001029",
+    "8694407158475_fba": "42001050",
+    "8694407192073_fba": "56001001",
+    "8694407192158_fba": "56001003",
+    "8694407192257_fba": "56001004",
+    "8694407192189_fba": "56001005",
+    "8694407192431_fba": "56001007",
+    "8694407192592_fba": "56001028",
+    "8694407192622_fba": "56001029",
+    "8694407192394_fba": "56001036",
+    "8694407192288_fba": "56001050",
+    "8694407200426_fba": "56010001",
+    "8694407200488_fba": "56010003",
+    "8694407200433_fba": "56010028",
+    "8694407212337_fba": "56010029"
 }
 
 # Rename columns based on their positions
@@ -117,12 +241,17 @@ def transform_customer_name(name):
         'ç': 'c', 'ğ': 'g', 'ı': 'i', 'ö': 'o', 'ş': 's', 'ü': 'u',
         'Ç': 'C', 'Ğ': 'G', 'İ': 'I', 'Ö': 'O', 'Ş': 'S', 'Ü': 'U'
     }
+    
+    # Check if the name is a string instance
+    if isinstance(name, str):
+        # Replace Turkish characters with English equivalents and convert to uppercase
+        for tr_char, en_char in char_mapping.items():
+            name = name.replace(tr_char, en_char)
+        return name.upper()
+    else:
+        # If name is not a string (possibly NaN), just return it as it is
+        return name
 
-    # Replace Turkish characters with English equivalents and convert to uppercase
-    for tr_char, en_char in char_mapping.items():
-        name = name.replace(tr_char, en_char)
-
-    return name.upper()
 
 def adjust_zip_code(zip_code):
     zip_code_str = str(zip_code)  # Ensure it's a string
@@ -131,12 +260,57 @@ def adjust_zip_code(zip_code):
     return zip_code_str
 
 def convert_date_format(date_str):
-    # Parse the date string to a datetime object
-    date_obj = pd.to_datetime(date_str)
+    try:
+        # Parse the date string to a datetime object
+        date_obj = pd.to_datetime(date_str)
 
-    # Format the datetime object to the desired string format
-    return date_obj.strftime('%d/%m/%Y')
+        # Format the datetime object to the desired string format
+        return date_obj.strftime('%d/%m/%Y')
+    except:
+        # If the date is NaT or invalid, return the original input
+        return date_str
 
+#LAST TWO COLUMNS ARE GONE???
+
+def consolidate_orders(df):
+    # Convert both 'ECOMM_ORD_NO' and 'SKU' to string type
+    df['ECOMM_ORD_NO'] = df['ECOMM_ORD_NO'].astype(str)
+    df['SKU'] = df['SKU'].astype(str)
+
+    # Create a new column that concatenates 'ECOMM_ORD_NO' and 'SKU'
+    df['ORDER_SKU'] = df['ECOMM_ORD_NO'] + '_' + df['SKU']
+    
+    # Group by this new column and sum the relevant columns
+    grouped = df.groupby('ORDER_SKU').agg({
+        'QUANTITY': 'sum',
+        'PRICE': 'sum',
+        'PRICE_TAX': 'sum',
+        'CARGO_COST': 'sum',
+        'CARGO_COST_TAX': 'sum',
+        'PROMOSYON_PRICE': 'sum',
+        'KARGO_PROMOSYON_INDIRIMI': 'sum'
+    }).reset_index()
+    
+    # Map back the summed values to the original DataFrame
+    df = df.drop_duplicates(subset='ORDER_SKU').drop(columns=['QUANTITY', 'PRICE', 'PRICE_TAX', 'CARGO_COST', 'CARGO_COST_TAX', 'PROMOSYON_PRICE', 'KARGO_PROMOSYON_INDIRIMI'])
+    df = df.merge(grouped, on='ORDER_SKU', how='left')
+    
+    # Remove the 'ORDER_SKU' as it has served its purpose
+    df.drop(columns=['ORDER_SKU'], inplace=True)
+    
+    return df
+
+def make_positive(value):
+    # If the value is less than zero, make it positive
+    if value < 0:
+        return -value
+    # Else, return the value as it is (covers zero and any unexpected positive values)
+    return value
+
+    # Function to get CATALOG_NO from SKU
+def get_catalog_no(sku):
+    return sku_to_catalog.get(sku, "Unknown")  # Returns 'Unknown' if SKU not found in dictionary
+ 
 # Abbreviations dictionary
 abbreviations = {
     "Caddesi": "Cd",
@@ -371,7 +545,7 @@ abbreviations = {
 }
 
 # Specify the data types for certain columns now that they are renamed
-df = df.astype({'ECOMM_CATALOG_NO': str, 'CAROG_TRACK_NO': str})
+df = df.astype({'ECOMM_CATALOG_NO': str, 'CARGO_TRACK_NO': str})
 
 df['CUSTOMER_NAME'] = df['CUSTOMER_NAME'].apply(transform_customer_name)
 df['INVOICE_ZIP_CODE'] = df['INVOICE_ZIP_CODE'].apply(adjust_zip_code)
@@ -382,6 +556,32 @@ df['PAYMENT_DATE'] = df['PAYMENT_DATE'].apply(convert_date_format)
 df['REPORT_DATE'] = df['REPORT_DATE'].apply(convert_date_format)
 df['DATE_ADDED'] = df['DATE_ADDED'].apply(convert_date_format)
 df['INVOICE_CITY'] = df['INVOICE_CITY'].str.upper()
+
+# Call the function to consolidate orders
+df = consolidate_orders(df)
+
+df['PROMOSYON_PRICE'] = df['PROMOSYON_PRICE'].apply(make_positive)
+
+# Apply the function to create a new column
+df['CATALOG_NO'] = df['SKU'].apply(get_catalog_no)
+
+#NET_PRICE CALCULATION 
+df['NET_PRICE'] = ((df['PRICE'] / df['QUANTITY']) - (df['PROMOSYON_PRICE'] / df['QUANTITY'])) / 1.2
+
+# Define the desired column order
+desired_order = [
+    'ECOMM_ORD_NO', 'ECOMM_CATALOG_NO', 'ORDER_DATE', 'PAYMENT_DATE', 
+    'REPORT_DATE', 'CUSTOMER_EMAIL', 'CUSTOMER_NAME', 'SKU', 
+    'CATALOG_DESCRIPTION', 'QUANTITY', 'CURRENCY', 'PRICE', 
+    'PRICE_TAX', 'CARGO_COST', 'CARGO_COST_TAX', 'CARGO_SRV_LEVEL', 
+    'INVOICE_ADDRESS_1', 'INVOICE_ADDRESS_2', 'INVOICE_CITY', 
+    'INVOICE_COUNTY', 'INVOICE_ZIP_CODE', 'INVOICE_COUNTRY', 
+    'KARGO_PROMOSYON_INDIRIMI', 'CAROG_TRACK_NO', 'SALES_CHANNEL', 
+    'STATE', 'DATE_ADDED', 'PROMOSYON_PRICE', 'CATALOG_NO', 'NET_PRICE'
+]
+
+# Reorder the columns in the DataFrame
+df = df.reindex(columns=desired_order)
 
 # Updated Process each row loop
 for index, row in df.iterrows():
